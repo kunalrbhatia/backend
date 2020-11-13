4e0f6a3ef9818fca4fc31588b05eace6e5ddf3e6
kunalrbhatia <kunal.bhatia@dailybooks.in>
2020-11-13 17:48:32 +0530

    commit

--- controllers/logincontroller.js
+++ controllers/logincontroller.js
@@ -1,20 +1,14 @@
 const express = require("express");
 const Users = require("../models/iconnect.model");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 var router = express.Router();
-
 router.post("/", async (req, res) => {
   const user = await Users.findOne({ email: req.body.email });
-  if (user) {
-    if (user.password === req.body.password) {
-      const token = jwt.sign({ _id: user.email }, "kunalbhatia");
-      res.header("auth-token", token).json(token);
-    } else {
-      res.status(400).send("Email or password are incorrect");
-    }
-  } else {
-    res.status(400).send("Email or password are incorrect");
-  }
+  if (!user) res.status(400).send("Email or password are incorrect");
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) res.status(400).send("Email or password are incorrect");
+  const token = jwt.sign({ _id: user.email }, "kunalbhatia");
+  res.header("auth-token", token).json(token);
 });
-
 module.exports = router;
--- models/iconnect.model.js
+++ models/iconnect.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 var usersSchema = new mongoose.Schema({
   password: { type: String },
   fname: { type: String },
@@ -6,5 +7,14 @@ var usersSchema = new mongoose.Schema({
   email: { type: String },
   token: { type: String },
 });
-
+usersSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (e) {
+    next(e);
+  }
+});
 module.exports = mongoose.model("Users", usersSchema);
